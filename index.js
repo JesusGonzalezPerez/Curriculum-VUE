c48175f846d16b27ad2dcedc6eb4a70e2b8efafd
JesusGonzalezPerez <jesusgpeligros@gmail.com>
2021-02-05 23:22:47 +0100

    Final commit

--- index.js
+++ index.js
@@ -4,39 +4,42 @@ var app = new Vue({
         message: '',
         ingles: true,
         show: false,
-        loader: true
+        preloading: true
+    },
+    components: {
+        loader: loader
     },
     mounted() {
+        this.preloading=false;
         let url = "json.json";
         axios.get(url)
             .then((response) => {
                 this.message = response.data;
-                this.loader=false;  
             })
             .catch((error) => {
                 console.log(error);
             })
     },
     methods: {
-        cambiarIdioma: function (){
-            if(this.ingles==true){
-                this.ingles=false;
-            }else{
-               this.ingles=true;
+        cambiarIdioma: function () {
+            if (this.ingles == true) {
+                this.ingles = false;
+            } else {
+                this.ingles = true;
             }
         },
-        openModal: function(index){
+        openModal: function (index) {
             $("#model").modal("show");
             $("#modalTitle").text(this.message.Curriculum.Projects[index].name);
-            $("#imgModal").attr("src",""+this.message.Curriculum.Projects[index].img+"");
-            $("#link1").attr("href",""+this.message.Curriculum.Projects[index].link+"");
-            $("#link2").attr("href",""+this.message.Curriculum.Projects[index].link2+"");
+            $("#imgModal").attr("src", "" + this.message.Curriculum.Projects[index].img + "");
+            $("#link1").attr("href", "" + this.message.Curriculum.Projects[index].link + "");
+            $("#link2").attr("href", "" + this.message.Curriculum.Projects[index].link2 + "");
             $("#description").text(this.message.Curriculum.Projects[index].description);
             $("#descripcion").text(this.message.Curriculum.Projects[index].descripcion);
             $("#tecno").text(this.message.Curriculum.Projects[index].technologies);
-        },
+        }
     },
     computed: {
-        
-    }
+
+}
 })
